362a09239a1e81fd8367280c1767f3b8075760d3
gufunkt-tacos <99559662+gufunkt-tacos@users.noreply.github.com>
2022-03-12 17:07:55 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,15 @@
+const mineflayer = require('mineflayer');
+var options = {
+  host: 'gufunkt.ploudos.me',
+  port: 25565,
+  username: 'Bot',
+};
+
+var bot = mineflayer.createBot(options);
+
+bot.on('spawn', () => { console.log("- Spawned -")
+  console.log("- Listening for messages -")
+  for (const player of Object.values(bot.players)) {
+    console.log("- Player " + player.displayName + " connected -")
+  }
+});
